fd63aa5e8fb151adb45530827482b048435bd904
iheeb9 <iheb.youssef@esprit.tn>
2022-07-25 18:12:24 +0200

    final

---
+++ src/shared/components/shared-detail-component/shared-detail-component.component.ts
@@ -0,0 +1,27 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { demandeModel } from 'shared/models/demandeModel';
+
+@Component({
+  selector: 'app-shared-detail-component',
+  templateUrl: './shared-detail-component.component.html',
+  styleUrls: ['./shared-detail-component.component.css']
+})
+export class SharedDetailComponentComponent implements OnInit {
+
+  constructor() { }
+@Input() demande: demandeModel
+@Input() aaceptrefus:boolean=false
+@Output() notif :EventEmitter<object> = new EventEmitter(); 
+  ngOnInit(): void {
+    console.log(this.demande)
+  }
+  
+decision(decision:object){
+ var response={}
+ response['decision']=decision
+ response['demande']=this.demande
+ this.notif.emit(response)
+
+}
+
+}
